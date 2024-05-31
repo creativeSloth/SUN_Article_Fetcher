460d0e5e5f13361ae801af16646ff1f921186053
creativeSloth <Eddischwanebeck@aol.com>
2024-05-31 09:40:15 +0200

    changed get_button_icons a bit

--- src/ui/widgets/buttons.py
+++ src/ui/widgets/buttons.py
@@ -8,20 +8,24 @@ from directories.directory_base import MAIN_PATHS
 
 def get_button_icons(self):
     icon_folder_path = MAIN_PATHS.dict['icons_folder_path']
+    normal_color_appendix = '_48dp_#13213B.png'
+    hover_color_appendix = '_48dp_#13213B95.png'
+    press_color_appendix = '_48dp_#13213B50.png'
 
     PB_MAP = {
         self.ui.load_articles_db_btn: (os.path.join(
-            icon_folder_path, 'database_48dp_#13213B.png'),
+            icon_folder_path, f'database{normal_color_appendix}'),
             os.path.join(
-            icon_folder_path, 'database_48dp_#13213B95.png'),
+            icon_folder_path, f'database{hover_color_appendix}'),
             os.path.join(
-            icon_folder_path, 'database_48dp_#13213B50.png')),
+            icon_folder_path, f'database{press_color_appendix}')),
+
         self.ui.load_articles_file_btn: (os.path.join(
-            icon_folder_path, 'home_storage_48dp_#13213B'),
+            icon_folder_path, f'home_storage{normal_color_appendix}'),
             os.path.join(
-            icon_folder_path, 'home_storage_48dp_#13213B95.png'),
+            icon_folder_path, f'home_storage{hover_color_appendix}'),
             os.path.join(
-            icon_folder_path, 'home_storage_48dp_#13213B50.png')),
+            icon_folder_path, f'home_storage{press_color_appendix}')),
     }
     return PB_MAP
 
