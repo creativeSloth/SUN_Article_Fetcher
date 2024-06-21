7381ceb11973bbf999051b8df00010040ab0c329
creativeSloth <Eddischwanebeck@aol.com>
2024-06-21 10:17:45 +0200

    changed make_exe

--- src/EXE/make_EXE.py
+++ src/EXE/make_EXE.py
@@ -14,32 +14,53 @@ def get_root_directory(path):
 items = []
 file_path = os.path.abspath(__file__)
 BASE_DIR = os.path.dirname(file_path)
-SOURCE_DIR = r"R:\_Projektrealisierung\_Dokumente\Doku\SUN Doc Fetcher\PyProjekt\SUN_Article_Fetcher-main\src\main.py"
+PARENT_DIR = os.path.dirname(BASE_DIR)
+SOURCE_DIR_root = PARENT_DIR
+SOURCE_DIR = os.path.join(SOURCE_DIR_root, "main.py")
 
-# Pfad zur Textdatei
+# Name des Programms
+exe_name = "SUN-DOC"
+
+# Pfad zur Textdatei in der alle zu übertragenden Ordner und deren Zielordner festgehalten sind
 paths_dir = os.path.join(BASE_DIR, "paths.txt")
+
+# Pfad
+icon_path = os.path.join(SOURCE_DIR_root, "ui", "icons", "launch.ico")
+
 
 # Pfade aus der Textdatei lesen
 with open(paths_dir, "r") as file:
     data_paths = file.readlines()
 
+# Der Name für die .exe-Datei
+
+
 # PyInstaller-Befehl aufbauen
-pyinstaller_command = ["pyinstaller", "--windowed", "--distpath", BASE_DIR]
+pyinstaller_command = [
+    "pyinstaller",
+    "--windowed",
+    "--distpath",
+    BASE_DIR,
+    "--name",
+    exe_name,
+    "--icon",
+    icon_path,
+]
 
 # Füge jedes --add-data Argument hinzu
 for path in data_paths:
     path = path.strip()  # Entferne Leerzeichen und Zeilenumbrüche
     src, dest_folder = path.split(";")
 
-    dest_dir = os.path.join(BASE_DIR, "main")
-    src_dir = os.path.join(BASE_DIR, "main", "_internal", dest_folder)
+    dest_dir = os.path.join(BASE_DIR, exe_name)
+    src_dir = os.path.join(BASE_DIR, exe_name, "_internal", dest_folder)
     pyinstaller_command.extend(["--add-data", f"{src}{os.pathsep}{dest_folder}"])
 
     # Bestimme das Wurzelverzeichnis des Zielordners
     src_dir_root = os.path.join(
-        BASE_DIR, "main", "_internal", get_root_directory(dest_folder)
+        BASE_DIR, exe_name, "_internal", get_root_directory(dest_folder)
     )
-    dest_dir_root = os.path.join(BASE_DIR, "main", get_root_directory(dest_folder))
+    dest_dir_root = os.path.join(BASE_DIR, exe_name, get_root_directory(dest_folder))
     items.append((src_dir_root, dest_dir_root))
 
 # Füge die Hauptdatei hinzu
@@ -58,6 +79,7 @@ for src_dir_root, dest_dir_root in items:
 
     if os.path.exists(src_dir_root):
         for item_name in os.listdir(src_dir_root):
+
             shutil.move(os.path.join(src_dir_root, item_name), dest_dir)
 
     # Entferne das leere Quellverzeichnis
